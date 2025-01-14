f822803ecc8710fe597ebcd6b6016523a358873f
Sasank Peetha <sasankpeetha7@gmail.com>
2025-01-14 20:12:19 +0000

    Removing File

--- Run/Script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# Exporting the environment variables
-export GI_TYPELIB_PATH="/usr/lib/girepository-1.0"
-export LD_LIBRARY_PATH="/usr/lib"
-
-# Running the fan control
- echo Simon8@ | sudo -S python /home/skywalker/Desktop/OFC/OFC.py > /dev/null 2>&1 &
